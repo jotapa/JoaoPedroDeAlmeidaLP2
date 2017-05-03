c0f15dc02fc12befb0c2ece723c785533eba9e64
jotapa <johnp_almeida@outlook.com>
2017-05-03 16:03:18 -0300

    Exercício da Aula de LP2: contar as vogais 03/05

---
+++ Exercícios de Aula/Aula de LP2 03-05/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string j;
+            int cont = 0;
+            j = Console.ReadLine().ToLower();
+
+            for(int i = 0; i < j.Length; i++)
+            {
+                if (j[i] == 'a')
+                    cont++;
+                else if (j[i] == 'e')
+                    cont++;
+                else if (j[i] == 'i')
+                    cont++;
+                else if (j[i] == 'o')
+                    cont++;
+                else if (j[i] == 'u')
+                    cont++;
+            }
+            Console.WriteLine("A mensagem tem {0} vogais", cont);
+
+        }
+    }
+}
---
+++ Exercícios de Aula/Correção Revisão de Subrotinas de 26-04/Correção Revisão de Subrotinas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correção_Revisão_de_Subrotinas
+{
+    class Program
+    {
+            public static void Main(string[] args)
+        {            
+            Console.WriteLine("Informe o número de carros do galpão");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Informe o modelo do carro");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Informe a quilometragem rodada");
+                double km = double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a potência do carro");
+                int poten = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(classificar(nome, km, poten));
+            }
+        }
+
+
+        public static string classificar(string modelo, double km, int pot)
+        {
+            string tipo, p;
+            
+            if (km <= 5000)
+                tipo = "novo";
+            else if (km <= 30000)
+                tipo = "semi novo";
+            else
+                tipo = "velho";
+            
+            if (pot > 200)
+                p = "potente";
+            else if (pot >= 120 && pot <= 200)
+                p = "forte";
+            else
+                p = "popular";
+            return String.Format("{0} - {1} - {2}", modelo, tipo, p);
+             
+        
+       }
+
+       
+    }
+}
