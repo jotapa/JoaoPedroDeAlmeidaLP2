b86697a8413571a5ad51d63bb1111df73585a85c
jotapa <johnp_almeida@outlook.com>
2017-05-17 15:49:39 -0300

    Exercício MeuIntParse 1ª parte (17/05)

---
+++ Exercícios de Aula/Aula de LP2 17-05/MeuIntParse/MeuIntParse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuIntParse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 * MeuIntParse(Console.ReadLine()));
+        }
+
+        public static int MeuIntParse(string nroComoStr)
+        {
+            
+
+        }
+    }
+}
