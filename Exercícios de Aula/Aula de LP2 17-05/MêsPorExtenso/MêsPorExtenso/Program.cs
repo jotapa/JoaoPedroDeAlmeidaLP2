d71e4872eb709dc587d6d44eb5398402b7d98ff9
jotapa <johnp_almeida@outlook.com>
2017-05-17 15:51:20 -0300

    Exercício MêsPorExtenso 1ª Parte (17/05)

---
+++ Exercícios de Aula/Aula de LP2 17-05/MêsPorExtenso/MêsPorExtenso/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MêsPorExtenso
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int d, m, a;
+            string data;
+
+            Console.WriteLine("Qual é a data?");
+            d = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+
+            
+
+            public static string Data (int meses)
+            {
+
+                
+            }
+
+        }
+    }
+}
