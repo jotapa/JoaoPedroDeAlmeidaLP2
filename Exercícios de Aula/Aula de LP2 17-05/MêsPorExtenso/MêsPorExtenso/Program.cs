3ae8858c5930f033b365b091f9a59c45cad5508d
jotapa <johnp_almeida@outlook.com>
2017-05-17 16:23:30 -0300

    Exercício MêsPorExtenso 2ª Parte (17/05)

--- Exercícios de Aula/Aula de LP2 17-05/MêsPorExtenso/MêsPorExtenso/Program.cs
+++ Exercícios de Aula/Aula de LP2 17-05/MêsPorExtenso/MêsPorExtenso/Program.cs
@@ -8,24 +8,72 @@ namespace MêsPorExtenso
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             int d, m, a;
             string data;
 
-            Console.WriteLine("Qual é a data?");
-            d = int.Parse(Console.ReadLine());
-            m = int.Parse(Console.ReadLine());
-            a = int.Parse(Console.ReadLine());
+            string dt = Console.ReadLine();
+            string sóMês = data.Substring(4,2);
 
             
-
-            public static string Data (int meses)
+            switch (m)
             {
+                case 01:
 
-                
+                    data = "janeiro";
+
+                    break;
+
+                case 02:
+                    data = "fevereiro";
+
+                    break;
+                   
+                case 03:
+                    
+                    data = "março";
+                    break;
+
+                case 04:
+                    
+                    data = "abril";
+                    break;
+
+                case 05:
+                    data = "maio";
+                    break;
+
+                case 06:
+                    data = "junho";
+                    break;
+
+                case 07:
+                    data = "julho";
+                    break;
+
+                case 08: 
+                    data = "agosto";
+                    break;
+
+                case 09:
+                    data = "setembro";
+                    break;
+
+                case 10:
+                    data = "outubro";
+                    break;
+
+                case 11:
+                    data = "novembro";
+                    break;
+
+                case 12:
+                    data = "dezembro";
+                    break;
+
             }
-
+            Console.WriteLine("{0} de {1} de {2}" d, m, a);
         }
     }
 }
