52e4d699fb5e8c73ed824d5b7ef685a5cdc8e689
João Pedro de Almeida <johnp_almeida@outlook.com>
2017-05-24 16:02:11 -0300

    Modificação para atualizar a quilometragem.

--- Exercícios de Aula/Correção Revisão de Subrotinas de 26-04/Correção Revisão de Subrotinas/Program.cs
+++ Exercícios de Aula/Correção Revisão de Subrotinas de 26-04/Correção Revisão de Subrotinas/Program.cs
@@ -8,47 +8,60 @@ namespace Correção_Revisão_de_Subrotinas
 {
     class Program
     {
+            public struct Carro
+            {
+                public string modelo;
+                public double km, AnoDeFabri;
+                public int pot;
+            }
             public static void Main(string[] args)
         {            
             Console.WriteLine("Informe o número de carros do galpão");
             int n = int.Parse(Console.ReadLine());
+            Carro[] carros = new Carro[n];    
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Informe o modelo do carro");
-                string nome = Console.ReadLine();
-                Console.WriteLine("Informe a quilometragem rodada");
-                double km = double.Parse(Console.ReadLine());
-                Console.WriteLine("Informe a potência do carro");
-                int poten = int.Parse(Console.ReadLine());
-
-                Console.WriteLine(classificar(nome, km, poten));
+                carros[i].modelo = Console.ReadLine(); 
+                carros[i].km = double.Parse(Console.ReadLine()); 
+                carros[i].pot = int.Parse(Console.ReadLine());
             }
+            
+                for (int i = 0; i < n; i++)
+                Console.WriteLine(Classificar(carros[i]));
         }
 
 
-        public static string classificar(string modelo, double km, int pot)
+        public static string Classificar(Carro c)
         {
             string tipo, p;
             
-            if (km <= 5000)
+            if (c.km <= 5000)
                 tipo = "novo";
-            else if (km <= 30000)
+            else if (c.km <= 30000)
                 tipo = "semi novo";
             else
                 tipo = "velho";
             
-            if (pot > 200)
+            if (c.pot > 200)
                 p = "potente";
-            else if (pot >= 120 && pot <= 200)
+            else if (c.pot >= 120 && c.pot <= 200)
                 p = "forte";
             else
                 p = "popular";
-            return String.Format("{0} - {1} - {2}", modelo, tipo, p);
+            return String.Format("{0} - {1} - {2}", c.modelo, tipo, p);
              
         
        }
-
+        public static void atualizarKm (Carro c1, double novo)
+        {
+            c1.km = novo;
+        }
+        
+        public static double TaxaDeUsoCarro (Carro c)
+        {
+            return c.km / (DateTime.Now.Year - c.AnoDeFabri);
+        }
        
     }
 }
