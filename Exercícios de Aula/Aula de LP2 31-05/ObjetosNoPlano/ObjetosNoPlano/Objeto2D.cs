a96c0fcc8179ed6ca59710ca621b2e0daa7ce633
João Pedro de Almeida <johnp_almeida@outlook.com>
2017-05-31 16:05:45 -0300

    Parte 1 exercício de objeto 2D

---
+++ Exercícios de Aula/Aula de LP2 31-05/ObjetosNoPlano/ObjetosNoPlano/Objeto2D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Objeto2D
+    {
+        public int x;
+        public int y;
+        public void Coordenadas()
+        {
+            x += 3;
+        }
+
+        public void Coordenadas()
+    }
+}
---
+++ Exercícios de Aula/Aula de LP2 31-05/ObjetosNoPlano/ObjetosNoPlano/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Objeto2D obj = new Objeto2D();
+
+            Console.Write("Informe a posição inicial de X: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe a posição inicial de Y: ");
+            int Y = int.Parse(Console.ReadLine());
+
+           
+
+            obj.x = Console.ReadLine();
+            obj.y = Console.ReadLine();
+            
+           
+                Console.Write("Informe o comando: ");
+            ConsoleKey comando = Console.ReadKey().Key;
+
+            if (comando == ConsoleKey.RightArrow)
+            {
+                obj.x += 
+            }
+        }
+    }
+}
