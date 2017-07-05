67c1ed7c3f5b89ff4997925d4b642ad5f79efa47
João Pedro Almeida <johnp_almeida@outlook.com>
2017-07-05 15:58:12 -0300

    Programa de Leitura Ótica e Livros da Hermione - Aula de 05/07

---
+++ 2C/2-LivrosHermione/2-LivrosHermione/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosHermione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titulo;
+            int diasdevol, npag;
+            Console.WriteLine("titulo do livro");
+            titulo = Console.ReadLine();
+            Console.WriteLine("Os dias restantes");
+            diasdevol =int.Parse(Console.ReadLine());
+
+
+
+
+
+        }
+    }
+}
