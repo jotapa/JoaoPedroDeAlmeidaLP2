f3ae4413e9b4b94834d044c12d5e88edc70e5998
jotapa <johnp_almeida@outlook.com>
2017-07-10 17:55:59 -0300

    Correção #1 no programa de Leitura Ótica

--- 2C/1-LeituraOtica/1-LeituraOtica/Program.cs
+++ 2C/1-LeituraOtica/1-LeituraOtica/Program.cs
@@ -32,82 +32,93 @@ namespace _1_LeituraOtica
                 Console.WriteLine("Qual o valor da alternativa E?");
                 e = int.Parse(Console.ReadLine());
 
-                if (a <= 100)
                 {
-                    resp = "A";
-                    certo++;
+                    if (a <= 100)
+                    {
+                        resp = "A";
+                        certo++;
 
+                    }
+                    else if (a >= 155)
+                    {
+                        amb++;
+                    }
+                    else
+                    {
+                        branco++;
+                    }
                 }
-                else if (a >= 155)
                 {
-                    amb++;
+                    if (b <= 100)
+                    {
+                        resp = "B";
+                        certo++;
+                    }
+                    else if (b >= 155)
+                    {
+                        amb++;
+                    }
+                    else
+                    {
+                        branco++;
+                    }
                 }
-                else
                 {
-                    branco++;
+                    if (c <= 100)
+                    {
+                        resp = "C";
+                        certo++;
+                    }
+                    else if (c >= 155)
+                    {
+                        amb++;
+                    }
+                    else
+                    {
+                        branco++;
+                    }
                 }
-                if (b <= 100)
                 {
-                    resp = "B";
-                    certo++;
+                    if (d <= 100)
+                    {
+                        resp = "D";
+                        certo++;
+                    }
+                    else if (d >= 155)
+                    {
+                        amb++;
+                    }
+                    else
+                    {
+                        branco++;
+                    }
                 }
-                else if (b >= 155)
                 {
-                    amb++;
-                }
-                else
-                {
-                    branco++;
-                }
-                if (c <= 100)
-                {
-                    resp = "C";
-                    certo++;
-                }
-                else if (c >= 155)
-                {
-                    amb++;
-                }
-                else
-                {
-                    branco++;
-                }
-                if (d <= 100)
-                {
-                    resp = "D";
-                    certo++;
-                }
-                else if (d >= 155)
-                {
-                    amb++;
-                }
-                else
-                {
-                    branco++;
-                }
-                if (e <= 100)
-                {
-                    resp = "E";
-                    certo++;
-                }
-                else if (e >= 155)
-                {
-                    amb++;
-                }
-                else
-                {
-                    branco++;
+                    if (e <= 100)
+                    {
+                        resp = "E";
+                        certo++;
+                    }
+                    else if (e >= 155)
+                    {
+                        amb++;
+                    }
+                    else
+                    {
+                        branco++;
 
+                    }
                 }
-                if(certo > 1 || branco  > 1 || amb > 4)
                 {
-                    Console.WriteLine(resp);
+                    if (certo >= 1 || branco >= 1 || amb > 4)
+                    {
+                        Console.WriteLine(resp);
+                    }
+                    else
+                    {
+                        Console.WriteLine("*");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("*");
-                }
-
 
             }
         }
