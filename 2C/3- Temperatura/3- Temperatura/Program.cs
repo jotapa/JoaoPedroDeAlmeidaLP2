28b323a9352c86ac2a7c4457c57e87676a58e549
João Pedro de Almeida <johnp_almeida@outlook.com>
2017-08-02 15:53:17 -0300

    Programa para Conversão de Temperatura de Celsius para Fahrenheit parte 1

---
+++ 2C/3- Temperatura/3- Temperatura/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3__Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            TemperaturaCelsius temp = new TemperaturaCelsius();
+            double convert = double.Parse(Console.ReadLine());
+            temp.Fahrenheit();
+        }
+    }
+}
---
+++ 2C/3- Temperatura/3- Temperatura/TemperaturaCelsius.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3__Temperatura
+{
+    class TemperaturaCelsius
+    {
+        public double c, f;
+
+        public double Fahrenheit();
+        {
+            f = 1.8 + 32;
+            f = f * c;
+
+            return f
+        }
+          
+
+    }
+}
