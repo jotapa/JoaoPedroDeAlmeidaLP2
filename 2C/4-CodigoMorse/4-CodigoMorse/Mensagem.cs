fbec0b3ba4b51f6bcfeffa2230181a547e93de4f
João Pedro de Almeida <johnp_almeida@outlook.com>
2017-08-09 15:56:03 -0300

    Programa de Codificação e Decodificação do Código Morse 09/08

---
+++ 2C/4-CodigoMorse/4-CodigoMorse/Mensagem.cs
@@ -0,0 +1,49 @@
+﻿,using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CodigoMorse
+{
+    class Mensagem
+    {
+        public string texto;
+        string[] tabela = new string [1]
+        {
+          ".-","-...","-.-.",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","...","-","..-","...-",".--", "-..-","-.--","--.."
+        };
+
+        char[] tabela2 = new char [26]
+        {
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+        };
+
+        public string codificar ()  
+        {
+                int i = 0;
+                int[] pos = new int[26];
+                while (i<texto.Length)
+                {
+                    pos[i] = texto[i] - 'A';
+                     i++;
+                }
+                i = 0;
+              
+         while (i<texto.Length)
+         {
+             retorno [i] = tabela[pos[i]
+         
+        }
+
+        public void decodificar ()
+        {
+         int i = 0;
+         while
+        }
+
+
+        }
+
+    }
+}
---
+++ 2C/4-CodigoMorse/4-CodigoMorse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CodigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            { 
+                int iniciar;
+                Mensagem texto = new Mensagem();
+                texto.texto = Console.ReadLine();
+                string[] exibir = new string[texto.texto.Length];
+                iniciar = int.Parse(Console.ReadLine());
+                if (iniciar == 1)
+                {
+                    exibir = texto.codificar();
+                    int i = 0;
+                    while(i>texto.texto.Length)
+                    {
+                        Console.WriteLine(exibir[i]);
+                    }
+                }
+        }
+    }
+}
