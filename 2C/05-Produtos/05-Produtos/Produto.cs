53eafe0680fca124b581a514b354b9330fa20151
João Pedro de Almeida <johnp_almeida@outlook.com>
2017-08-16 16:03:00 -0300

    Aula de 16/08 com Raphael Muniz e Nathália Prozenko -  Programa do Produto

---
+++ 2C/05-Produtos/05-Produtos/Produto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Produtos
+{
+    class Produto
+    {
+       private int id;
+        public int Id 
+        {
+            get { return id; }
+        }
+        public double preco { get; set; }
+      
+        private string nome;
+        public string Nome {
+            get { return nome; }
+        }
+        public int Quant;
+
+        public Produto (int id ,string nome , double preco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.preco = preco;
+            this.Quant = 0;
+        }
+        public void Receber(int qtd)
+        {
+            
+                Quant += qtd;
+            
+        }
+         public void Retirada (int qtd)
+        {
+             if (Quant >=qtd)
+             {
+                 Quant -= qtd;
+             }
+             else
+             {
+                 throw new Exception();
+
+             } 
+
+        }
+        public string Imprimir()
+         {
+             return String.Format("ID {0} - NOME {1} - PREÇO {2;0,00} ", id, nome, preco,Quant);
+         }
+    }
+}
---
+++ 2C/05-Produtos/05-Produtos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto(1, "sabonete 60g", 1.49);
+            p.Receber(100);
+            p.Retirada(20);
+            Console.WriteLine(p.Imprimir());
+
+
+
+
+
+        }
+    }
+}
